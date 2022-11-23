60befa766dae557d2ac324193a40418241d4e4ba
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-23 17:08:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import './App.css';
+/**
+ *  Main app Component 
+ */
+
+// Import SASS
+import './styles/App.scss';
+
+// Import routes dependencies
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+import Register from './pages/Register' 
 
 function App() {
-  return (<></>);
+    // Logic
+    // JSX
+    return (
+        <BrowserRouter>
+            {/* Route Group */}
+            <Routes>
+                {/* Parent Route */}
+                <Route path="/" element={<Layout />}>
+                    {/* Default route set using index attribute */}
+                    <Route index element={<Home />} />
+                    <Route path="home" element={<Home />} />
+
+                    {/* Normal path */}
+                    <Route path="register" element={<Register />} />
+
+                    {/* Undefined URLs */}
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 /**
