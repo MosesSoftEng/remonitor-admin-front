3a4a7928a0b6ae47d33d416dad5dfbdfe99b92be
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-23 17:08:07 +0300

    Create NoPage.js

---
+++ src/pages/NoPage.js
@@ -0,0 +1,16 @@
+/**
+ * 404 page
+ * @returns JSX template view
+ */
+export default function NoPage() {
+    // JSX view
+    return (
+        <>
+            <h1>You got lost!</h1>
+            
+            <p>
+                Go to <a href="/">home page</a>
+            </p>
+        </>
+    );
+};
