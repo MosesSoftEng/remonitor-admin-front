06d99057c7e7744bdfb5d3cd0b1c1cbd69e23d4c
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-01 08:55:40 +0300

    Add authorization check

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom"
+
 /**
  * Home page
  * @returns JSX template view
  */
- export default function Dash() {
+export default function Dash(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Reidrect user to register if not logged in.
+        if (props.token === null) {
+            navigate('/register');
+        }
+    }, [navigate, props]);
+
     // JSX view
-    return(
-    <>
-    <h1>Dash</h1>
-    </>
+    return (
+        <>
+            <h1>Dash</h1>
+        </>
     );
 };
