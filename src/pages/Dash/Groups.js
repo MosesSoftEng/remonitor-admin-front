bc84e1e95958b74f22065aec645743b81055cd0d
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-01 12:02:00 +0300

    Create Groups.js

---
+++ src/pages/Dash/Groups.js
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom"
+// import AuthToken from '../utils/AuthToken'
+
+/**
+ * Dashboard Groups page.
+ * @returns JSX template view
+ */
+export default function Groups(props) {
+    // const navigate = useNavigate();
+    // const { token } = AuthToken();
+
+    // // Reidrect user to register if not logged in.
+    // if (token === null) {
+    //     navigate('/register');
+    // }
+
+    // JSX view
+    return (
+        <>
+            <h1>Dash Groups</h1>
+        </>
+    );
+};
