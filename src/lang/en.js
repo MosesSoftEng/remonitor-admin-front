249bdc714526350a17c8a55db1452d4596a2f4dc
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-05 10:59:14 +0300

    Update en.js

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,5 +1,6 @@
 export default function en() {
     return {
-        users_create_button_text_default: 'Create user'
+        users_create_button_text_default: 'Create user',
+        users_group_select_default_option_text: 'Select user\'s group'
     }
 };
