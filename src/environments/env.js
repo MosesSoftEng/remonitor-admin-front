2fd22dca9be26c91cf951a39bb4d3e3c6b8b7d73
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-05 12:53:34 +0300

    Add email pattern validation to global variables

--- src/environments/env.js
+++ src/environments/env.js
@@ -5,7 +5,8 @@
 // Named exports
 const APP_NAME = 'Remonitor';
 const API_URL = 'http://localhost:3003';
+const EMAIL_PATTERN_VALIDATION = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export {
-    APP_NAME, API_URL
+    APP_NAME, API_URL, EMAIL_PATTERN_VALIDATION
 };
