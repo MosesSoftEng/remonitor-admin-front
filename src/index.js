e8b98940313a9cda3168ad3d30a1c69bcf00d8d7
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-12 14:39:25 +0300

    Commented out strictmode

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Render to root element.
 root.render(
-  <React.StrictMode>
+//   <React.StrictMode>
     <App />
-  </React.StrictMode>
+//   </React.StrictMode>
 );
 
 
