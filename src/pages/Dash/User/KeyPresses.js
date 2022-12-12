e004da2d3b0996bd3d3f4855a6b7737ff85d2996
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-12 12:30:22 +0300

    Add function to get user keypresses

--- src/pages/Dash/User/KeyPresses.js
+++ src/pages/Dash/User/KeyPresses.js
@@ -1,3 +1,27 @@
+    const apiGetUserKeyPresses = function () {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow'
+        };
+
+        fetch(`${API_URL}/user/key-presses/${props.token}/${client.userId}`, requestOptions)
+            .then(function (response) {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+
+                if (data.success) {
+                    setKeyPresses(data.data);
+                }
+            }).catch(function (error) {
+                props.showToast(`Connection error`);
+            });
+    }
+        apiGetUserKeyPresses();
         </>
     );
 }
