260ccb6b3fc57f7eca8eec549924df89b62361fb
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-13 15:46:04 +0300

    Create UserLayout.js

---
+++ src/pages/Dash/User/UserLayout.js
@@ -0,0 +1,52 @@
+import { NavLink, Outlet, useParams } from "react-router-dom";
+
+export default function UserLayout() {
+    const { clientData } = useParams();
+    const client = JSON.parse(clientData);
+
+    return (
+        <>
+            <br />
+            <div className="container">
+                <h1>User: {client.name}</h1>
+
+                <div className="row">
+                    <div className="col-sm-12">
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item">
+                                <NavLink end to={`/dash/user/user/${JSON.stringify(client)}`} className={function ({ isActive }) {
+                                    return (isActive) ? "nav-link active" : "nav-link"
+                                }}>
+                                    Summary
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink end to={`/dash/user/user/${JSON.stringify(client)}/keypresses`} className={function ({ isActive }) {
+                                    return (isActive) ? "nav-link active" : "nav-link"
+                                }}>
+                                    Keypresses
+                                </NavLink>
+                            </li>
+
+                            
+                            <li className="nav-item">
+                                <NavLink end to={`/dash/user/user/${JSON.stringify(client)}/screenshots`} className={function ({ isActive }) {
+                                    return (isActive) ? "nav-link active" : "nav-link"
+                                }}>
+                                    Screenshots
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Sessions</a>
+                            </li>
+                        </ul>
+
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
