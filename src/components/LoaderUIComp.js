1224268addfba27db7abad42d6c49e9bf93fd7fb
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-13 06:48:24 +0300

    Create LoaderUIComp.js

---
+++ src/components/LoaderUIComp.js
@@ -0,0 +1,13 @@
+/**
+ * Module to display loading.
+ * @returns JSX UI
+ */
+export default function LoaderUIComp() {
+    return (
+        <>
+            <div className="text-center">
+                loading...
+            </div> : ''
+        </>
+    );
+}
