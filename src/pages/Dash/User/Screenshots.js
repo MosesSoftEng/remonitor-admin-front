4c86c5d50eaad5b87b7cd224abb9f435820715e8
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-13 11:32:14 +0300

    Add image modal functionality

--- src/pages/Dash/User/Screenshots.js
+++ src/pages/Dash/User/Screenshots.js
@@ -14,6 +14,8 @@ export default function Screenshots(props) {
     const apiGetUserScreenshots = function () {
         setFetchingData(true);
 
+        console.log('apiGetUserScreenshots');
+
         const requestOptions = {
             method: 'GET',
             headers: {
@@ -42,6 +44,16 @@ export default function Screenshots(props) {
         return `${_date.getDate()}, ${_date.getMonth() + 1} ${_date.getFullYear()} ${_date.getHours()}:${('0' + _date.getMinutes()).slice(-2)}`;
     }
 
+    /*
+     * Modal
+     */
+    const [imageModalLink, setImageModalLink] = useState('');
+
+    const setImageModalLinkFun = function (imageLink) {
+        setImageModalLink(imageLink);
+    }
+
+
     useEffect(() => {
         apiGetUserScreenshots();
     }, []);
@@ -54,7 +66,9 @@ export default function Screenshots(props) {
                 <div className="row">
                     {screenshots.map((screenshot, index) => (
                         <div key={index} className="col-sm-4 text-center">
-                            <img className="img-fluid" src={`https://s3-137627469964-remonitor-screenshots.s3.amazonaws.com/${encodeURIComponent(screenshot.id)}`} alt="Image Alt" loading="lazy" />
+                            <img
+                                onClick={function () { setImageModalLinkFun(`https://s3-137627469964-remonitor-screenshots.s3.amazonaws.com/${encodeURIComponent(screenshot.id)}`) }}
+                                className="img-fluid" src={`https://s3-137627469964-remonitor-screenshots.s3.amazonaws.com/${encodeURIComponent(screenshot.id)}`} alt="Image Alt" loading="lazy" />
 
                             <br />
 
@@ -63,9 +77,19 @@ export default function Screenshots(props) {
                     ))}
                 </div>
 
-                <LoaderUIComp show={isFetchingData}/>
-
+                <LoaderUIComp show={isFetchingData} />
             </div>
+
+            {/* Image modal */}
+            {imageModalLink === '' ? '' :
+                <div className="app-image-modal">
+                    <button onClick={function () { setImageModalLinkFun(''); }} className="btn btn-primary rounded-pill m-3">
+                        <i className="bi bi-x-circle"></i>
+                    </button>
+
+                    <img className="img-fluid" src={imageModalLink} alt="Image Alt" loading="lazy" />
+                </div>
+            }
         </>
     );
 }
