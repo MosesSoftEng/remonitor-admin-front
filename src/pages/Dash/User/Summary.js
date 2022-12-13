27e2e83e013612900ec65d14cf3fc57b63f230bb
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-13 17:03:23 +0300

    Update Summary.js

--- src/pages/Dash/User/Summary.js
+++ src/pages/Dash/User/Summary.js
@@ -1,39 +1,61 @@
 import { API_URL } from "../../../environments/env";
 
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LoaderUIComp from "../../../components/LoaderUIComp";
+import CopyToClipBoard from "../../../components/CopyToClipBoard";
 
 export default function Summary(props) {
     const { clientData } = useParams();
-    const navigate = useNavigate();
     const client = JSON.parse(clientData);
 
     return (
         <>
             <br />
             <div className="container">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">
 
-                        </h5>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">
 
-                        <p className="card-text">
-                            Group:<br />
-                            {client.groupId}<br />
-                            <br />
+                                </h5>
 
-                            email:<br />
-                            {client.email} <br />
-                            <br />
+                                <div className="card-text">
 
-                            passkey:<br />
-                            {client.paswordResetCode}<br />
-                            <br />
+                                    <div className="row">
+                                        <div className="col-lg-3 text-center">
+                                            <i className="bi bi-person-circle" style={{ fontSize: '96px' }}></i>
+                                            <br />
+                                            {client.name}
+                                        </div>
 
-                            <small>{client.description}</small>
-                        </p>
+                                        <div className="col-lg-9">
+                                            <div className="row">
+                                                <div className="col-12 col-xl-3"><b><i className="bi bi-people"></i> Group</b></div>
+                                                <div className="col-12 col-xl-9">{client.groupId} <CopyToClipBoard textToCopy={client.groupId}/></div>
+                                            </div>
+
+                                            <div className="row">
+                                                <div className="col-12 col-xl-3"><b><i className="bi bi-at"></i> email</b></div>
+                                                <div className="col-12 col-xl-9">{client.email} <CopyToClipBoard textToCopy={client.email}/></div>
+                                            </div>
+
+                                            <div className="row">
+                                                <div className="col-12 col-xl-3"><b><i className="bi bi-key"></i> passkey</b></div>
+                                                <div className="col-12 col-xl-9">{client.paswordResetCode} <CopyToClipBoard textToCopy={client.paswordResetCode}/></div>
+                                            </div>
+
+                                            <hr />
+                                            {client.description}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6">
                     </div>
                 </div>
             </div>
