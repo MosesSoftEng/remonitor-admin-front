641fe3ef29cafbbc04b2ae289c63f1d0234d69fe
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-16 23:45:38 +0300

    Update env.js

--- src/environments/env.js
+++ src/environments/env.js
@@ -4,8 +4,8 @@
 
 // Named exports
 const APP_NAME = 'Remonitor';
-// const API_URL = 'http://localhost:3003';
-const API_URL = 'https://edxwj7m3we.execute-api.us-east-1.amazonaws.com/dev';
+const API_URL = 'http://localhost:3003';
+// const API_URL = 'https://edxwj7m3we.execute-api.us-east-1.amazonaws.com/dev';
 const EMAIL_PATTERN_VALIDATION = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export {
