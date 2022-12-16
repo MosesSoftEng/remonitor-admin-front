d33bc22f2966d4e4d6ab25c4b72e71fa6c3c7816
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-16 23:45:47 +0300

    Update environment.js

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,7 +1,8 @@
 // Default export
 const environment = {
     appName: 'Remonitor',
-    apiURL: 'https://edxwj7m3we.execute-api.us-east-1.amazonaws.com/dev'
+    // apiURL: 'https://edxwj7m3we.execute-api.us-east-1.amazonaws.com/dev'
+    apiURL: 'http://localhost:3003'
 };
 
 export default environment;
