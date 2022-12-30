ff18fc8f5e711e2d8df080ed12f386ab7c81ee29
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-30 21:19:52 +0300

    Add function to check for invalid password

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,14 +1,23 @@
-import { EMAIL_PATTERN_VALIDATION } from "../environments/env";
+import { EMAIL_PATTERN_VALIDATION, PASSWORD_PATTERN_VALIDATION } from "../environments/env";
 
 /**
  * IsEmailInValid - function to chec kif email is invalid.
  * @param {*} email 
- * @returns true if email is invalid, otherwise true
+ * @returns true if email is invalid, otherwise false
  */
 const IsEmailInValid = function (email) {
   return !EMAIL_PATTERN_VALIDATION.test(email);
 };
 
+/**
+ * IsEmailInValid - function to chec kif email is invalid.
+ * @param {*} password 
+ * @returns true if password is invalid, otherwise false
+ */
+const IsPasswordInValid = function (password) {
+  return !PASSWORD_PATTERN_VALIDATION.test(password);
+};
+
 export {
-  IsEmailInValid
+  IsEmailInValid, IsPasswordInValid
 }
