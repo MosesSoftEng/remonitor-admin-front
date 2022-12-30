ca02ccdd971cd261c2843995f29455e1b0d322da
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-30 16:33:15 +0300

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -2,30 +2,36 @@ import { useState, useEffect } from 'react';
 import { Toast } from 'bootstrap';
 
 /**
- * Module to show toast
+ * _Toast - Functional component to show toasts.
+ *
  * @returns JSX UI
  */
 export default function _Toast(props) {
-        /* Toast */
-    // Setup Toast
-    const [toast, setToast] = useState(null);
+  // Setup Toast
+  const [toast, setToast] = useState(null);
 
-    // After JSX has rendered.
-    useEffect(() => {
-        setToast(new Toast(document.getElementById('toast')));
-    }, []);
+  // Setup Toast
+  useEffect(() => {
+    setToast(new Toast(document.getElementById('toast')));
+  }, []);
 
-    return (
-        <>
-            {/* TODO: Turn toast to a component*/}
-            <div id="toast" className="toast align-items-center text-white bg-primary border-0 position-absolute bottom-0 end-0 m-2" role="alert" aria-live="assertive" aria-atomic="true">
-                <div className="d-flex">
-                    <div className="toast-body">
-                        {props.toastMessage}
-                    </div>
-                    <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>
-        </>
-    );
+  // Shhow Toast
+  useEffect(() => {
+    if (props.toastMessage !== '') {
+      toast.show();
+    }
+  }, [toast, props.toastMessage]);
+
+  return (
+    <>
+      <div id="toast" className="toast align-items-center text-white bg-primary border-0 position-absolute bottom-0 end-0 m-2" role="alert" aria-live="assertive" aria-atomic="true">
+        <div className="d-flex">
+          <div className="toast-body">
+            {props.toastMessage}
+          </div>
+          <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+      </div>
+    </>
+  );
 }
