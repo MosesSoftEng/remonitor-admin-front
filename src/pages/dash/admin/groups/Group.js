5ac056fd4e6e4959b8b07e31cb52eb5706ed0196
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-09 07:20:29 +0300

    Create Group.js

---
+++ src/pages/dash/admin/groups/Group.js
@@ -0,0 +1,12 @@
+/**
+ * Show group.
+ * @returns JSX template view
+ */
+export default function Group(props) {
+    // JSX view
+    return (
+        <>
+            Group Details
+        </>
+    );
+};
