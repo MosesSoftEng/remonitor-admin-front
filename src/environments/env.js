2593a8dceed9f71ac7d5a83d1ed52a4615be7f00
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-09 09:43:46 +0300

    Add dev env variable

--- src/environments/env.js
+++ src/environments/env.js
@@ -2,6 +2,8 @@
  * Module for Global variables.
  */
 
+const DEV_STAGE = 'dev';
+
 // Named exports
 const APP_NAME = 'Remonitor';
 const API_URL = 'http://localhost:3003';
@@ -10,6 +12,7 @@ const EMAIL_PATTERN_VALIDATION = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 const PASSWORD_PATTERN_VALIDATION = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 export {
+  DEV_STAGE,
   APP_NAME,
   API_URL,
   EMAIL_PATTERN_VALIDATION,
